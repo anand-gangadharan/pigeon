4448561e73622b4dce34e98b191259726240cc92
qwiselabs <qwiselabs@faircodetech.com>
2024-07-01 13:32:03 +0530

    Added stack delete and cf validation

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 STACK_NAME=pigeon
-TEMPLATE=file:////Users/anandg/git/aws-cloudfront-exp/simple-lambda/deploy-cloudformation.yaml 
+TEMPLATE=file:///Users/anandgangadharan/git/pigeon/deploy-cloudformation.yaml 
 CAPS=CAPABILITY_NAMED_IAM
 
 create() {
@@ -8,18 +8,28 @@ create() {
 }
 
 update() {
-    aws cloudformation update-stack --stack-name $STACK_NAME --template-body $TEMPLATE --capabilities $CAPS
+    aws cloudformation update-stack --stack-name $STACK_NAME - $TEMPLATE --capabilities $CAPS
+}
+validate() {
+    aws cloudformation validate-template  --template-body $TEMPLATE  
+}
+delete() {
+    aws cloudformation delete-stack --stack-name $STACK_NAME
 }
 
 case "$1" in 
     create)
        create
        ;;
+    validate)
+       validate
+       ;;
+    delete)
+       delete
+       ;;
     update)
        update
        ;;
     *)
-       echo "Usage: $0 {create|update}"
+       echo "Usage: $0 {create|update|validate|delete}"
 esac
-
-exit 0 
